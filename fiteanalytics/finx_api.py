c6c6e77e1cb4fde745cb0a90f29bd789c53d08a0
Jake Mathai <jakemathai@jakes-mbp.home>
2021-04-14 19:25:52 -0700

    finx

--- fiteanalytics/finx_api.py
+++ fiteanalytics/finx_api.py
@@ -19,22 +19,16 @@ from concurrent.futures import ThreadPoolExecutor
 DEFAULT_API_URL = 'https://sandbox.finx.io/api/'
 
 
-def get_cache_key(params):
+def _get_cache_key(params):
     return ','.join([f'{key}:{params[key]}' for key in sorted(params.keys())])
 
 
-class __SyncFinX:
+class _FinX:
 
     def __init__(self, **kwargs):
         """
-        Client constructor supports multiple methods for passing credentials
-
-        :keyword finx_api_key: string (handle with care)
-        :keyword finx_api_endpoint: string
-        :keyword yaml_path: string
-        :keyword env_path: string
-
-        If yaml_path not passed, loads env_path (if passed) then checks environment variables
+        Client constructor - supports keywords finx_api_key and finx_api_endpoint, or FINX_API_KEY and FINX_API_ENDPOINT
+        environment variables
         """
         self.__api_key = kwargs.get('finx_api_key')
         self.__api_url = kwargs.get('finx_api_endpoint')
@@ -46,12 +40,12 @@ class __SyncFinX:
                             'environment variable FINX_API_KEY')
         if self.__api_url is None:
             self.__api_url = DEFAULT_API_URL
-        self.session = session()
         self.max_cache_size = kwargs.get('max_cache_size')
         if self.max_cache_size is None:
             self.max_cache_size = 100
         self.cache = LRU(self.max_cache_size)
-        self.executor = ThreadPoolExecutor()
+        self._session = session() if kwargs.get('session') else None
+        self._executor = ThreadPoolExecutor() if kwargs.get('no_executor') else None
 
     def get_api_key(self):
         return self.__api_key
@@ -64,19 +58,20 @@ class __SyncFinX:
         collect()
         return None
 
-    def dispatch(self, request_body, **kwargs):
+    def _dispatch(self, request_body, **kwargs):
+        assert self._session is not None
         request_body['finx_api_key'] = self.__api_key
         if any(kwargs):
             request_body.update({
                 key: value for key, value in kwargs.items()
                 if key != 'finx_api_key' and key != 'api_method' and value is not None
             })
-        cache_key = get_cache_key(request_body)
+        cache_key = _get_cache_key(request_body)
         cached_response = self.cache.get(cache_key)
         if cached_response is not None:
             print('Found in cache')
             return cached_response
-        data = self.session.post(self.__api_url, data=request_body).json()
+        data = self._session.post(self.__api_url, data=request_body).json()
         error = data.get('error')
         if error is not None:
             print(f'API returned error: {error}')
@@ -88,14 +83,14 @@ class __SyncFinX:
         """
         List API methods with parameter specifications
         """
-        return self.dispatch({'api_method': 'list_api_functions'}, **kwargs)
+        return self._dispatch({'api_method': 'list_api_functions'}, **kwargs)
 
     def get_security_reference_data(self, security_id, **kwargs):
         """
         Security reference function
 
         :param security_id: string
-        :param as_of_date: string as YYYY-MM-DD (optional)
+        :keyword as_of_date: string as YYYY-MM-DD (optional)
         """
         request_body = {
             'api_method': 'security_reference',
@@ -104,7 +99,7 @@ class __SyncFinX:
         as_of_date = kwargs.get('as_of_date')
         if as_of_date is not None:
             request_body['as_of_date'] = as_of_date
-        return self.dispatch(request_body, **kwargs)
+        return self._dispatch(request_body, **kwargs)
 
     def get_security_analytics(self, security_id, **kwargs):
         """
@@ -121,7 +116,7 @@ class __SyncFinX:
         :keyword cap_gain_short_tax: float (optional)
         :keyword cap_gain_long_tax: float (optional)
         """
-        return self.dispatch({
+        return self._dispatch({
             'api_method': 'security_analytics',
             'security_id': security_id,
             'use_kalotay_analytics': False
@@ -136,7 +131,7 @@ class __SyncFinX:
         :keyword price: float (optional)
         :keyword shock_in_bp: int (optional)
         """
-        return self.dispatch({
+        return self._dispatch({
             'api_method': 'security_cash_flows',
             'security_id': security_id
         }, **kwargs)
@@ -147,44 +142,42 @@ class __SyncFinX:
         :param function: Client member function to invoke for each security
         :param security_args: Dict mapping dict mapping security_id (string) to a dict of key word arguments
         """
-        assert function != self.get_api_methods and type(security_args) is dict and len(security_args) < 100
-        tasks = [self.executor.submit(function, security_id=security_id, **kwargs)
+        assert self._executor is not None \
+            and function != self.get_api_methods \
+            and type(security_args) is dict \
+            and len(security_args) < 100
+        tasks = [self._executor.submit(function, security_id=security_id, **kwargs)
                  for security_id, kwargs in security_args.items()]
         return [task.result() for task in tasks]
 
 
-class __AsyncFinx(__SyncFinX):
+class _AsyncFinx(_FinX):
 
     def __init__(self, **kwargs):
         """
-        Client constructor accepts 2 distinct methods for passing credentials named FINX_API_KEY and FINX_API_ENDPOINT
-
-        :keyword yaml_path: path to YAML file
-        :keyword env_path: path to .env file
-
-        If yaml_path not passed, loads env_path (if passed) then checks environment variables
+        Client constructor - supports keywords finx_api_key and finx_api_endpoint, or FINX_API_KEY and FINX_API_ENDPOINT
+        environment variables
         """
-        super().__init__(**kwargs)
+        super().__init__(**kwargs, session=False, executor=False)
         self.__api_key = self.get_api_key()
         self.__api_url = self.get_api_url()
-        del self.executor
-        self.session = None
+        self._session = None
 
-    async def dispatch(self, request_body, **kwargs):
+    async def _dispatch(self, request_body, **kwargs):
         request_body['finx_api_key'] = self.__api_key
-        if self.session is None:
-            self.session = ClientSession()
+        if self._session is None:
+            self._session = ClientSession()
         if any(kwargs):
             request_body.update({
                 key: value for key, value in kwargs.items()
                 if key != 'finx_api_key' and key != 'api_method' and value is not None
             })
-        cache_key = get_cache_key(request_body)
+        cache_key = _get_cache_key(request_body)
         cached_response = self.cache.get(cache_key)
         if cached_response is not None:
             print('Found in cache')
             return cached_response
-        async with self.session.post(self.__api_url, data=request_body) as response:
+        async with self._session.post(self.__api_url, data=request_body) as response:
             data = await response.json()
             error = data.get('error')
             if error is not None:
@@ -197,14 +190,14 @@ class __AsyncFinx(__SyncFinX):
         """
         List API methods with parameter specifications
         """
-        return await self.dispatch({'api_method': 'list_api_functions'})
+        return await self._dispatch({'api_method': 'list_api_functions'})
 
     async def get_security_reference_data(self, security_id, **kwargs):
         """
         Security reference function
 
         :param security_id: string
-        :param as_of_date: string as YYYY-MM-DD (optional)
+        :keyword as_of_date: string as YYYY-MM-DD (optional)
         """
         request_body = {
             'api_method': 'security_reference',
@@ -213,7 +206,7 @@ class __AsyncFinx(__SyncFinX):
         as_of_date = kwargs.get('as_of_date')
         if as_of_date is not None:
             request_body['as_of_date'] = as_of_date
-        return await self.dispatch(request_body)
+        return await self._dispatch(request_body)
 
     async def get_security_analytics(self, security_id, **kwargs):
         """
@@ -230,7 +223,7 @@ class __AsyncFinx(__SyncFinX):
         :keyword cap_gain_short_tax: float (optional)
         :keyword cap_gain_long_tax: float (optional)
         """
-        return await self.dispatch({
+        return await self._dispatch({
             'api_method': 'security_analytics',
             'security_id': security_id,
             'use_kalotay_analytics': False
@@ -245,7 +238,7 @@ class __AsyncFinx(__SyncFinX):
         :keyword price: float (optional)
         :keyword shock_in_bp: int (optional)
         """
-        return await self.dispatch({
+        return await self._dispatch({
             'api_method': 'security_cash_flows',
             'security_id': security_id,
         }, **kwargs)
@@ -266,20 +259,16 @@ class __AsyncFinx(__SyncFinX):
         return await asyncio.gather(*tasks)
 
 
-class __FinXSocket(__SyncFinX):
+class _FinXSocket(_FinX):
 
     def __init__(self, **kwargs):
         """
-        Client constructor supports multiple methods for passing credentials
-
-        :keyword finx_api_key: string (handle with care)
-        :keyword finx_api_endpoint: string
+        Client constructor - supports keywords finx_api_key and finx_api_endpoint, or FINX_API_KEY and FINX_API_ENDPOINT
+        environment variables
         """
-        super().__init__(**kwargs)
+        super().__init__(**kwargs, session=False)
         self.__api_key = super().get_api_key()
         self.__api_url = super().get_api_url()
-        self.session.close()
-        del self.session
         self.is_authenticated = False
 
         def on_open(socket):
@@ -317,7 +306,7 @@ class __FinXSocket(__SyncFinX):
         thread = Thread(target=self.__socket.run_forever, kwargs={'ping_interval': 60}, daemon=True)
         thread.start()
 
-    def await_result(self, cache_key, callback, **kwargs):
+    def _await_result(self, cache_key, callback, **kwargs):
         try:
             result = None
             while result is None:
@@ -326,7 +315,7 @@ class __FinXSocket(__SyncFinX):
         except:
             print(traceback.format_exc())
 
-    def dispatch(self, payload, **kwargs):
+    def _dispatch(self, payload, **kwargs):
         if not self.is_authenticated:
             i = 5000
             while not self.is_authenticated and i >= 1:
@@ -340,7 +329,7 @@ class __FinXSocket(__SyncFinX):
                 key: value for key, value in kwargs.items()
                 if key != 'finx_api_key' and key != 'api_method' and value is not None
             })
-        cache_key = get_cache_key(payload)
+        cache_key = _get_cache_key(payload)
         cached_response = self.cache.get(cache_key)
         if cached_response is not None:
             print('Found in cache')
@@ -350,7 +339,7 @@ class __FinXSocket(__SyncFinX):
         payload['cache_key'] = cache_key
         self.__socket.send(json.dumps(payload))
         if callable(callback):
-            self.executor.submit(self.await_result, cache_key, callback, **kwargs)
+            self._executor.submit(self._await_result, cache_key, callback, **kwargs)
         return cache_key
 
     def batch(self, function, security_args, **kwargs):
@@ -358,6 +347,7 @@ class __FinXSocket(__SyncFinX):
         Invoke function for batch of securities
         :param function: Client member function to invoke for each security
         :param security_args: Dict mapping dict mapping security_id (string) to a dict of key word arguments
+        :keyword callback: function to invoke on each corresponding result when received
         """
         assert function != self.get_api_methods and type(security_args) is dict and len(security_args) < 100
         return [function(security_id, **_security_args, **kwargs)
@@ -371,17 +361,20 @@ def FinX(**kwargs):
     """
     kind = kwargs.pop('kind', 'sync')
     if kind == 'socket':
-        return __FinXSocket(**kwargs)
+        return _FinXSocket(**kwargs)
     if kind == 'async':
-        return __AsyncFinx(**kwargs)
-    return __SyncFinX(**kwargs)
+        return _AsyncFinx(**kwargs)
+    return _FinX(**kwargs)
 
 
 """
+import time
+import asyncio
 from timeit import timeit
+from finx_api import FinX
+from asgiref.sync import AsyncToSync
 
 def sync_client_test():
-    from finx_api import FinX
     finx = FinX()
     print('\n' + '*'*20 + 'GET API METHODS' + '*'*20 + '\n')
     finx.get_api_methods()
@@ -400,34 +393,28 @@ def sync_client_test():
             '912796F61': {'as_of_date': '2021-04-01'}
         }
     )
-    finx.session.close()
-"""
-"""
-timeit(lambda: sync_client_test(), number=1)
-"""
-"""
-from asgiref.sync import AsyncToSync
+    finx._session.close()
+    
 
 @AsyncToSync
 async def async_client_test():
-    from finx_api import FinX
     finx = FinX(kind='async')
+    tasks = []
     print('\n' + '*'*20 + 'GET API METHODS' + '*'*20 + '\n')
-    await finx.get_api_methods(
-        callback=lambda x, **kwargs: print(f'\n====> GOT API METHODS: {x}\n'))
+    tasks.append(finx.get_api_methods(
+        callback=lambda x, **kwargs: print(f'\n====> GOT API METHODS: {x}\n')))
     print('\n' + '*'*20 + 'GET SECURITY ANALYTICS' + '*'*20 + '\n')
-    await finx.get_security_analytics(
+    tasks.append(finx.get_security_analytics(
         '9127962F5', 
         as_of_date='2021-03-24', 
-        callback=lambda x, **kwargs: print(f'\n====> GOT SECURITY ANALYTICS: {x}\n'))
+        callback=lambda x, **kwargs: print(f'\n====> GOT SECURITY ANALYTICS: {x}\n')))
     print('\n' + '*'*20 + 'GET SECURITY CASH FLOWS' + '*'*20 + '\n')
-    await finx.get_security_cash_flows(
+    tasks.append(finx.get_security_cash_flows(
         '9127962F5', 
         as_of_date='2021-03-24', 
-        callback=lambda x, **kwargs: print(f'\n====> GOT SECURITY CASH FLOWS: {x}\n'))
-    finx.clear_cache()
+        callback=lambda x, **kwargs: print(f'\n====> GOT SECURITY CASH FLOWS: {x}\n')))
     print('\n' + '*'*20 + 'BATCH SECURITY ANALYTICS' + '*'*20 + '\n')
-    await finx.batch(
+    tasks.append(finx.batch(
         finx.get_security_analytics, 
         {
             '9127962F5': {'as_of_date': '2021-03-24', 'foo': 'bar'},
@@ -435,18 +422,12 @@ async def async_client_test():
             '912796B24': {'as_of_date': '2021-04-01'},
             '912796F61': {'as_of_date': '2021-04-01'}
         }
-    )
-    await finx.session.close()
-"""
-"""
-timeit(lambda: async_client_test(), number=1)
-"""
-"""
-from timeit import timeit
-import time
-
+    ))
+    await asyncio.gather(*tasks)
+    await finx._session.close()
+    
+    
 def socket_client_test():
-    from finx_api import FinX
     finx = FinX(kind='socket')
     keys = []
     print('\n' + '*'*20 + 'GET API METHODS' + '*'*20 + '\n')
@@ -466,16 +447,30 @@ def socket_client_test():
         }
     )
     i = 15000
-    results = {key: finx.cache.get(key) for key in keys}
-    remaining_tasks = {key: value for key, value in results.items() if value is None}
+    remaining_tasks = {key: finx.cache.get(key) for key in keys}
+    keys = []
+    for key, value in remaining_tasks.items():
+        if value is not None:
+            print(f'Got {key}')
+        else:
+            keys.append(key)
     while any(remaining_tasks) and i >= 1:
         time.sleep(0.001)
         i -=1
-        results = {key: finx.cache.get(key) for key in remaining_tasks}
-        remaining_tasks = {key: value for key, value in results.items() if value is None}
+        remaining_tasks = {key: finx.cache.get(key) for key in keys}
+        keys = []
+        for key, value in remaining_tasks.items():
+            if value is not None:
+                print(f'Got {key}')
+            else:
+                keys.append(key)
     if any(remaining_tasks):
-        print(f'Didn\'t get results in time for {len(remaining_tasks.keys())} out of {len(keys)} tasks')
+        print(f'Didn\'t get results in time for {len(remaining_tasks.keys())} tasks')
+        
+        
 """
 """
-timeit(lambda: socket_client_test(), number=1)
+timeit(lambda: sync_client_test(), number=30)
+timeit(lambda: async_client_test(), number=30)
+timeit(lambda: socket_client_test(), number=30)
 """
--- fiteanalytics/finx_api_example.py
+++ fiteanalytics/finx_api_example.py
@@ -3,41 +3,39 @@
 finx_api_example.py
 """
 import json
-import sys
+from fiteanalytics.finx_api import FinX
 
-from fiteanalytics import finx_api
+# Initialize synchronous client with no arguments -
+# checks environment variables for credentials
+finx = FinX()
 
+# Get API methods
+print('\n*********** API Methods ***********')
+api_methods = finx.get_api_methods()
+print(json.dumps(api_methods, indent=4))
 
-if __name__ == "__main__":
-    """
-    optional command line arguments:
-    argv[0] = security_id
-    argv[1] = as_of_date
-    """
+security_id = 'USQ98418AH10'
+as_of_date = '2020-09-14'
 
-    # Initialize client
-    finx_client = finx_api.FinX()
+# Get security reference data
+print('\n*********** Security Reference Data ***********')
+reference_data = finx.get_security_reference_data(
+    security_id,
+    as_of_date=as_of_date)
+print(json.dumps(reference_data, indent=4))
 
-    # Get API methods
-    print('\n*********** API Methods ***********')
-    api_methods = finx_client.get_api_methods()
-    print(json.dumps(api_methods, indent=4))
+# Get security analytics
+print('\n*********** Security Analytics ***********')
+analytics = finx.get_security_analytics(
+    security_id,
+    as_of_date=as_of_date,
+    price=100)
+print(json.dumps(analytics, indent=4))
 
-    # GET (OPTIONAL) COMMAND LINE ARGUMENTS
-    security_id = sys.argv[1] if len(sys.argv) > 1 else 'USQ98418AH10'
-    as_of_date = sys.argv[2] if len(sys.argv) > 2 else '2020-09-14'
-
-    # Get security reference data
-    print('\n*********** Security Reference Data ***********')
-    reference_data = finx_client.get_security_reference_data(security_id, as_of_date=as_of_date)
-    print(json.dumps(reference_data, indent=4))
-
-    # Get security analytics
-    print('\n*********** Security Analytics ***********')
-    analytics = finx_client.get_security_analytics(security_id, as_of_date=as_of_date, price=100)
-    print(json.dumps(analytics, indent=4))
-
-    # Get projected cash flows
-    print('\n*********** Security Cash Flows ***********')
-    cash_flows = finx_client.get_security_cash_flows(security_id, as_of_date=as_of_date, price=100)
-    print(json.dumps(cash_flows, indent=4))
+# Get projected cash flows
+print('\n*********** Security Cash Flows ***********')
+cash_flows = finx.get_security_cash_flows(
+    security_id,
+    as_of_date=as_of_date,
+    price=100)
+print(json.dumps(cash_flows, indent=4))
