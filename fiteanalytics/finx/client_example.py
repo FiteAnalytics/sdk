6f763b2c86cde8f3880092ba779b9a4d8e13613c
Jake Mathai <jakemathai@jakes-mbp.home>
2021-04-21 10:03:28 -0700

    sock

--- fiteanalytics/finx/client_example.py
+++ fiteanalytics/finx/client_example.py
@@ -7,11 +7,11 @@ from fiteanalytics.finx.client import FinXClient
 
 # Initialize synchronous HTTP client 
 # Credentials fetched from environment variables
-finx = FinXClient()
+finx = FinXClient('socket')
 
 # Get API methods
 print('\n*********** API Methods ***********')
-api_methods = finx.get_api_methods()
+api_methods = finx.get_api_methods(block=True)
 print(json.dumps(api_methods, indent=4))
 
 security_id = 'USQ98418AH10'
@@ -21,7 +21,8 @@ as_of_date = '2020-09-14'
 print('\n*********** Security Reference Data ***********')
 reference_data = finx.get_security_reference_data(
     security_id, 
-    as_of_date=as_of_date)
+    as_of_date=as_of_date,
+    block=True)
 print(json.dumps(reference_data, indent=4))
 
 # Get security analytics
@@ -29,7 +30,8 @@ print('\n*********** Security Analytics ***********')
 analytics = finx.get_security_analytics(
     security_id, 
     as_of_date=as_of_date, 
-    price=100)
+    price=100,
+    block=True)
 print(json.dumps(analytics, indent=4))
 
 # Get projected cash flows
@@ -37,7 +39,8 @@ print('\n*********** Security Cash Flows ***********')
 cash_flows = finx.get_security_cash_flows(
     security_id, 
     as_of_date=as_of_date, 
-    price=100)
+    price=100,
+    block=True)
 print(json.dumps(cash_flows, indent=4))
 
 # Batch get security reference data
@@ -52,5 +55,6 @@ finx.batch_security_reference(
             'security_id': '3133XXP50',
             'as_of_date': '2020-09-14'
         }
-    ]
+    ],
+    block=True
 )
