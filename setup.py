dd04025177b0daf6ba59794ab46b4d6a857769dc
Jake Mathai <jakemathai@jakes-mbp.home>
2021-04-21 10:13:50 -0700

    sock

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='fiteanalytics',
-    description='FinX API SDK',
-    version='2.0.2',
-    author='Fite Analytics LLC',
-    author_email='info@fiteanalytics.com',
-    classifiers=['License :: OSI Approved :: GNU Affero General Public License v3',],
-    license='APGL3',
-    packages=find_packages(exclude=('*.tests',)),
-    url='https://github.com/FiteAnalytics/sdk',
-    install_requires=[x.split('==')[0] for x in open('requirements.txt').readlines()],
-    # include_package_data is needed to reference MANIFEST.in
-    include_package_data=True,
-)
+with open('requirements.txt') as requirements:
+    setup(
+        name='fiteanalytics',
+        description='FinX API SDK',
+        version='2.0.2',
+        author='Fite Analytics LLC',
+        author_email='info@fiteanalytics.com',
+        classifiers=['License :: OSI Approved :: GNU Affero General Public License v3',],
+        license='APGL3',
+        packages=find_packages(exclude=('*.tests',)),
+        url='https://github.com/FiteAnalytics/sdk',
+        install_requires=[x.split(next((sep for sep in ['==', '>=', '<=', '~='] if sep in x), ''))[0]
+                          for x in requirements.readlines()],
+        # include_package_data is needed to reference MANIFEST.in
+        include_package_data=True,
+    )
