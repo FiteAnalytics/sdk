0df96ed57dbda882cbe13e481e225c2dbaf12ff1
dick-mule <dick@fiteanalytics.com>
2021-05-17 00:19:58 -0700

    socket

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as requirements:
     setup(
         name='fiteanalytics',
         description='FinX API SDK',
-        version='2.0.8',
+        version='2.0.1',
         author='Fite Analytics LLC',
         author_email='info@fiteanalytics.com',
         classifiers=['License :: OSI Approved :: GNU Affero General Public License v3',],
